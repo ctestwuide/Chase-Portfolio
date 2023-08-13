d3ca692161e72b9698d819a0a20de8f6c956e417
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-08-13 09:45:52 -0500

    added styling and jsx to nav

--- my-app/src/components/header/CTA.jsx
+++ my-app/src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../assets/Chase Testwuide Resume.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={Resume} className="btn">Download Resume</a>
+        <a href={Resume} target='_blank' className="btn">Download Resume</a>
         <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   )
--- my-app/src/components/nav/Nav.jsx
+++ my-app/src/components/nav/Nav.jsx
@@ -6,16 +6,21 @@ import {MdOutlineSchool} from 'react-icons/md'
 import {MdWorkOutline} from 'react-icons/md'
 import {FiFolder} from 'react-icons/fi'
 import {LuContact} from 'react-icons/lu'
+import {useState} from 'react'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#education"><MdOutlineSchool /></a>
-      <a href="#experience"><MdWorkOutline /></a>
-      <a href="#portfolio"><FiFolder /></a>
-      <a href="#contact"><LuContact /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#education" onClick={() => setActiveNav('#education')} className={activeNav === '#' ? 'active' : ''}><MdOutlineSchool /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#' ? 'active' : ''}><MdWorkOutline /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#' ? 'active' : ''}><FiFolder /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#' ? 'active' : ''}><LuContact /></a>
     </nav>
   )
 }
