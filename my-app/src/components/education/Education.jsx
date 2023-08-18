26ce8750b3ee35cc17fad4330318764032a56864
ctestwuide <117029690+ctestwuide@users.noreply.github.com>
2023-08-18 11:04:16 -0500

    added education and portfolio section

--- my-app/src/components/education/Education.jsx
+++ my-app/src/components/education/Education.jsx
@@ -1,10 +1,108 @@
 import React from 'react'
 import './education.css'
+import {FaCheck} from 'react-icons/fa'
 
 const Education = () => {
   return (
     <section id="education">
-      Education
+      <h5>My Educational Background</h5>
+      <h2>Accounting ○ Finance ○ Marketing ○ Software Development</h2>
+
+      <div className="container education__container">
+        <article className="education">
+          <div className="education__head">
+            <h3>Univeristy of Wisconsin</h3>
+          </div>
+
+          <ul className="education__list">
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            
+          </ul>
+
+        </article>
+
+        <article className="education">
+          <div className="education__head">
+            <h3>Univeristy of Glasgow</h3>
+          </div>
+
+          <ul className="education__list">
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            
+          </ul>
+
+        </article>
+
+        <article className="education">
+          <div className="education__head">
+            <h3>Cornell Univeristy</h3>
+          </div>
+
+          <ul className="education__list">
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            <li>
+              <FaCheck className='education__list-icon' />
+              <p>random text here about what I lerned at Madison</p>
+            </li>
+            
+          </ul>
+
+        </article>
+
+      </div>
     </section>
   )
 }
--- my-app/src/components/portfolio/Portfolio.jsx
+++ my-app/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,88 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from "../../assets/torginol_logo.jpeg"
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      Portfolio
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Torginol logo" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribble.com/Alien-pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+
+        </article>
+
+
+      </div>
     </section>
   )
 }
