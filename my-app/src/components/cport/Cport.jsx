b5e7b56fef675e86d5cd86b7a04f920ef35d233e
Chase Testwuide <ctestwuide@gmail.com>
2023-08-29 01:14:42 +0000

    updated coding portfolio

--- my-app/src/components/cport/Cport.jsx
+++ my-app/src/components/cport/Cport.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './cport.css'
 import Journey from '../../assets/Untitled.mp4'
 import Snake from '../../assets/snake.png'
+import Admin from '../../assets/Admin.png'
 
 
 // Import Swiper React components
@@ -63,16 +64,15 @@ const Cport = () => {
         <SwiperSlide autoHeight={false} className="testimonial">
         <article className="portfolio__item">
             <div className="portfolio__item-image">
-            <video src={Journey} controls width="100%" height="auto"></video>
+            <img src={Admin} alt="Software Engineering Admin Assignment" />
             </div>
-            <h3>Journey Joiner Dissertation Project</h3>
-            <p>Journey Joiner stands as the pinnacle project for my master's studies at the University of Glasgow, serving as a testament to 
-                the skills I honed throughout the course. While much of the program emphasized Java, I was eager to pivot towards web 
-                application development for my dissertation, aiming to diversify my skill set. This venture allowed me to refine my proficiency 
-                in React and delve deeper into Django for backend integration. Crafting this app was an exhilarating experience! It's conceived 
-                to assist individuals in locating travel buddies, addressing the challenges posed by mismatched schedules and budgetary constraints</p>
+            <h3>Designing Part of a Course Admin System</h3>
+            <p>In my Software Engineering course, I developed an app designed to function as a university database for tracking 
+                instructors. The system allows Course Directors to list teaching vacancies, specify educational 
+                prerequisites for professors, and enables administrators to fill these roles. Instead of interfacing with a live database, 
+                the program interacts with CSV files that simulate real-world database functionality. I got an 'A' in case you were wondering! </p>
             <div className="portfolio__item-cta">
-            <a href='https://github.com/ctestwuide/Journey-Joiner' className='btn btn-primary' target='_blank' rel="noopener noreferrer">GitHub</a>
+            <a href='https://github.com/ctestwuide/AE2-Software-Engineering' className='btn btn-primary' target='_blank' rel="noopener noreferrer">GitHub</a>
             </div>
         </article>
         </SwiperSlide>
