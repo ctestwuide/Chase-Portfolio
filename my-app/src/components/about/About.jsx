cf6ffdb4614781152367912bea3afcdbfff0043f
Chase Testwuide <ctestwuide@gmail.com>
2023-08-30 01:04:46 +0000

    finished

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -44,15 +44,23 @@ const About = () => {
           </div>
 
           <p>
-          In the last five years within the manufacturing industry, I've faced many challenges and have refined my expertise as a Business Development Manager. 
-          As I approach the completion of my Master's degree in Business from Cornell University in December 2023, 
-          I am exporing new career opportunities. My focus spans across several sectors including
-          accounting, finance, analytics, consulting, and 
-          software development. I'm receptive to roles both domestically in the US and on the international stage, 
-          and I hold the necessary rights to work in the UK.
-          </p>
+            <strong className='about-thing'>Job Search:</strong> Seeking a new full-time position for January 2024 <br/> 
+            <strong className='about-thing'>Target Roles:</strong> Finance (Private Equity), Accounting (Tax), Consulting, Business Development, Software Development <br/>
+            <strong className='about-thing'>My Ideal Company:</strong> Startups! I love getting my hands dirty in a variety of areas within
+             a company. My diverse skillset and education give me the necessary competencies to successfully attack projects in
+            a variety of areas. I'm also interested in working for large, innovative companies where I can learn best practices and witness new industry trends. <br/>
+              
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            <strong className='about-thing'>Current Employment: </strong>  
+            In the last five years within the manufacturing industry, I've faced many challenges and have refined my expertise as a Business Development Manager at Torginol. 
+            As I approach the completion of my Master's degree in Business from Cornell University in December 2023, 
+            I'm eager to change the trajectory of my career with a new challenge. My focus spans across several sectors including
+            accounting, finance, analytics, consulting, and 
+            software development. I'm receptive to roles both domestically in the US and on the international stage, 
+            and I hold the necessary rights to work in the UK.
+            </p>
+
+          <a href="#contact" className='btn btn-primary'>Interested in hiring me? Let's Talk</a>
 
         </div>
 
