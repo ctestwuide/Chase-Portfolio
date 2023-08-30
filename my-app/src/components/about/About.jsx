fe7ba4750396b30feecd415c71f7b033fa04c9eb
Chase Testwuide <ctestwuide@gmail.com>
2023-08-30 01:05:39 +0000

    tiny edit

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={Cat} alt="me and a cat image" />
+            <img src={Cat} alt="me and a cat" />
           </div>
         </div>
 
